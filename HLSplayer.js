244d6a030c81b767ebcbc26e5fa2acf16ce87104
Theonlypablo56 <enriqueperezm56@gmail.com>
2024-07-09 12:59:52 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/Mkscode92/AiQworkflow1-""
    This reverts commit 3e0922194c047244246e63f6d0b3c27f42db79e8.

---
+++ HLSplayer.js
@@ -0,0 +1,153 @@
+const EnglLink = {
+    VideoLink: "https://dfflvukqjg5l4.cloudfront.net/Leo1080p_English/Leo1080p_English_with_CC720pLeoEnglishModified.m3u8",
+    SubtitleLink: "languages/English.vtt",
+    label: 'English',
+
+
+}; // English
+
+const SpanLink = {
+    VideoLink: "https://dfflvukqjg5l4.cloudfront.net/Leo1080p_Spanish/Leo1080p_Spanish - Made with Clipchamp720pLeoSpanishModified.m3u8",
+    SubtitleLink: "languages/Spanish.vtt",
+    label: 'Spanish',
+
+
+};  //Spanish
+
+const VietLink =  {
+    VideoLink: "https://dfflvukqjg5l4.cloudfront.net/Leo1080p_Viet/LeoVietDubLeoVietnameseModified.m3u8",
+    SubtitleLink: "languages/Vietnamese.vtt",
+    label: 'Vietnamese',
+};//Viet
+
+
+function AddAudio(AudioElement,player,Presets){ 
+
+    if (Presets.NetflixUI) {  //Allows for Netflix Caption UI, Turn on for Multisource, off for HLS
+        var AudioOption = document.createElement("button");
+
+        AudioOption.classList = "AudioOption";
+        AudioOption.textContent = Presets.label;
+
+        var DisplayHeight = String(15 * (AudioElement.childNodes.length + 1)) + "%";
+        
+        AudioOption.style.top = DisplayHeight;
+        AudioElement.append(AudioOption);
+    }
+
+    AudioOption.addEventListener('click', function() {
+        var TimeStamp = player.currentTime();
+        hls.loadSource(Presets.src);
+        player.currentTime(TimeStamp);
+        player.play();
+    }) 
+    
+    
+}
+
+function AddSubtitle(SubtitleElement,player,Presets) {
+
+    var SubtitleOption = document.createElement("button");
+    SubtitleOption.classList = "SubtitleOption";
+    SubtitleOption.textContent = Presets.label;
+
+    let index = player.textTracks().length;
+
+    var DisplayHeight = String(15 * (index + 1)) + "%";
+    SubtitleOption.style.top = DisplayHeight;
+    SubtitleElement.append(SubtitleOption);
+
+    player.addRemoteTextTrack(Presets);
+
+
+
+    SubtitleOption.addEventListener('click', function() {
+        for (let i = 0; i < player.textTracks().length; i++) {
+            player.textTracks()[i].mode = 'hidden';
+        }
+        player.textTracks()[index].mode = 'showing';
+    })
+}
+
+function HLSVideo(ParentElement,Source, NetflixUI) { //ParentNode , Source Array, if (Bool = false) NetflixUI = false
+    // Create a video element
+    var video = document.createElement('video');
+    var hls = new Hls();
+
+    // Set styles
+    video.className = "video-js";
+    video.type = 'application/x-mpegURL';
+
+    // Append the video element to the parent element first
+    ParentElement.appendChild(video);
+
+    // Then, initialize the video.js player with the video element
+    const player = videojs(video, { controls: true });
+
+    player.ready(function() {
+        // Add a caption track
+        if (NetflixUI) {
+            var controlBar = player.controlBar;
+            controlBar.removeChild('seekToLive');
+            controlBar.removeChild('pictureInPictureToggle');
+
+            var button = controlBar.getChild('subsCapsButton');
+            var coverDiv = document.createElement('div');
+            var ExitButton = document.createElement('button');
+            var AudioList = document.createElement('div');
+            var SubtitleList = document.createElement('div');
+
+            coverDiv.classList = "LanguagePanel";
+            ExitButton.classList = "exitButton";
+            AudioList.classList = "AudioList";
+            SubtitleList.classList = "SubtitleList";
+
+            // Append the cover div to the video element
+            ParentElement.append(coverDiv);
+            coverDiv.append(ExitButton);
+            coverDiv.append(AudioList);
+            coverDiv.append(SubtitleList);
+
+            // Add event listener to the button
+            button.on('click', function() {
+                // Toggle the visibility of the cover div when the button is clicked
+                coverDiv.style.display = 'block';
+                coverDiv.style.pointerEvents = 'all';
+            });
+            ExitButton.addEventListener('click', function() {
+                coverDiv.style.display = 'none';
+                coverDiv.style.pointerEvents = 'none';
+            })
+            ExitButton.textContent = "X";
+
+            AddSubtitle(SubtitleList, player, {
+                kind: 'captions',
+                label: 'Off',
+                src: ''
+            })
+
+            for (let i = 0; i < Source.length; i++) {
+                AddAudio(AudioList, player, {
+                    label: Source[i].label,
+                    src: Source[i].VideoLink,
+                    NetflixUI: true
+                })
+                
+                AddSubtitle(SubtitleList, player, {
+                    kind: 'captions',
+                    label: Source[i].label,
+                    src: Source[i].SubtitleLink,
+                    NetflixUI: true
+                })
+            }
+        }
+        
+    });
+
+    // Hls
+
+    hls.loadSource(Source[0].VideoLink);
+    hls.attachMedia(video);
+    window.hls = hls;
+
+}
--- Syncronize.js
+++
@@ -1,5 +0,0 @@
-var MainVideo = document.getElementById("Avideo");
-var VideoB = document.getElementById("Bvideo");
-var VideoC = document.getElementById("Cvideo");
-var VideoD = document.getElementById("Dvideo");
-var select = document.getElementById("select");
