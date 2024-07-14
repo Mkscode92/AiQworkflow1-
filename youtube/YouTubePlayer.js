39ed7cc35eca61c892668eeac7ed57c0dc42e971
Theonlypablo56 <enriqueperezm56@gmail.com>
2024-07-14 15:31:46 -0500

    HUGEEE Youtube page restyling and function update.
    YouTube Page changes/additions:
    
    1. Changed the color scheme into a more blue-ish theme.
    
    2. Gave every player a Youtube link input which is animated so that it hides itself visually and physically when played and then pops back out when paused. (Can add a setting to always keep it out)
    
    3. Created a top bar and a side bar that pops out when menu button is pushed, which will hold the drop in players that'll be coded in a later commit.
    
    4.  CODE CHANGES:
    
    4.1. Updated the plyr object into an actual object to store other info.
    
    4.2. Added "plyrInputLinker" so that anyone can easily link a YouTube link input to a player.
    
    4.3. Added show/hide Input functions and show/hide side bar functions so that those elements can easily be closed or opened with a single function.
    
    Next commit is hopefully going to be more code refactoring in order to make player changes or additions much more easier and readable with object methods.
    
    El Fin.

--- youtube/YouTubePlayer.js
+++ youtube/YouTubePlayer.js
@@ -1,6 +1,7 @@
 const MediaBox = document.getElementById("MainMediaBox");
+const defaultSettings = {debug: 'true', ratio:'16:9'};
 let checks = true;
-
+let allPlyrs = [];
 //var link = "https://www.youtube.com/embed/eI4an8aSsgw";
 //let vidLink = "https://youtu.be/ErTef06r0R0?si=2KKESTwhoy0QR4rl";         //all three types of YT links
 //let vidLink = "https://www.youtube.com/watch?v=SlBOpNLFUC0&list=LL&index=1";
@@ -38,7 +39,7 @@ const vidIDGrabber = (link) => {
       break;
     default:                                // Just a regular Error message.
       console.log(`Youtube link not recognized, ${check1}, ${check2}, ${check3}`);
-      return;
+      return false;
   }
   return videoID;
 }
@@ -59,8 +60,12 @@ const plyrSetup = (idOrSelector = '#player', vidSrc = 'https://youtu.be/tPWioxKV
   let iframe = iframeRoot.getElementsByClassName('youtube_Iframe')[0];
   iframe.src = vidSrc;
   
-  let player = new Plyr(idOrSelector, vidSettings); // video player settings are the same as Plyr's settings
-  return player;
+  let info = {
+    plyr: new Plyr(idOrSelector, vidSettings), // video player settings are the same as Plyr's settings,
+    vidIframe: iframe,
+    inputElement: "Nuthin"
+  };
+  return info;
 }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 
 const iframeCheck = (idOrSelector) => {
@@ -90,12 +95,109 @@ const quickID = (idName) => {
   return document.getElementById(idName); // I got tired ok.
 }
 
+const plyrInputLinker = (plyrObj, inputElement) => {
+  inputElement.addEventListener("change", () => {
+    //console.log(inputElement.value);
+    let vidId = vidIDGrabber(inputElement.value); 
+    if (!vidId){return}
+
+    plyrObj.plyr.source = {
+      type: 'video',
+      sources: [
+        {
+          src: vidId,
+          provider: 'youtube',
+        },
+      ],
+    };
+  });
+  plyrObj.inputElement = inputElement;
+}
+
+const showInput = (plyrObj) => {
+  let inputElement = plyrObj.inputElement;
+  let classList = inputElement.classList;
+
+  inputElement.classList.add('inputDropAnim');
+  classList.contains('inputDropAnim_reversed') ? inputElement.classList.remove('inputDropAnim_reversed') : "";
+
+  inputElement.addEventListener('animationend', () => {
+    inputElement.classList.remove('inputDropAnim');
+  });
+  console.log('paused');
+}
+
+const hideInput = (plyrObj) => {
+  let inputElement = plyrObj.inputElement;
+  let classList = inputElement.classList;
+
+  inputElement.classList.add('inputDropAnim_reversed');
+  classList.contains('inputDropAnim') ? inputElement.classList.remove('inputDropAnim') : "";
+
+  console.log('playing');
+}
+
+const hideSideBar = () => {
+  let sideBar = quickID('sideBar');
+  sideBar.classList.add('sideBarClosing');
+  sideBar.classList.contains('sideBarOpening') ? sideBar.classList.remove('sideBarOpening') : "";
+  
+  quickID('sideOpenBtn').disabled = true;
+  sideBar.addEventListener('animationend', () => {quickID('sideOpenBtn').disabled = false;});
+}
+
+const showSideBar = () => {
+  let sideBar = quickID('sideBar');
+  if (sideBar.classList.contains('sideBarOpening')){
+    hideSideBar();
+    return;
+  }
+  sideBar.classList.add('sideBarOpening');
+  sideBar.classList.contains('sideBarClosing') ? sideBar.classList.remove('sideBarClosing') : "";
+  
+  quickID('sideOpenBtn').disabled = true;
+  sideBar.addEventListener('animationend', () => {quickID('sideOpenBtn').disabled = false;});
+}
+
+const divOutliner = () => {
+  let all = document.getElementsByTagName("*");
+  console.log(all.length);
+  for (let m=0; m < all.length; m++) {
+    let randomColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+    all[m].style.outline = `1px solid ${randomColor}`
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 //      Every player should always be wrapped in a DOMContentLoaded 
 //     listener, if not then it might run before page is even loaded which
 //    means causes code to try to find an element that doesn't yet exist.
-  let plyr1 = plyrSetup(quickID('player'), "https://youtu.be/tPWioxKVNe4?si=jzFw0GBHQMJ6wp-A", {debug: true});
-  let plyr2 = plyrSetup(quickID('SidePlayer1'), "https://www.youtube.com/watch?v=sf7TC0poGlg", {debug: true});
-  let plyr3 = plyrSetup(quickID('SidePlayer2'), "https://www.youtube.com/watch?v=SlBOpNLFUC0&list=LL&index=1", {debug: true});
-  let plyr4 = plyrSetup(quickID('SidePlayer3'), "https://www.youtube.com/watch?v=Fjp2TdlTTIU", {debug: true});
+  //window.scrollTo(0, quickID('top').clientHeight);
+  window.scrollTo(0, 0);
+  
+  let plyr1 = plyrSetup(quickID('MainMediaBox'), "https://youtu.be/tPWioxKVNe4?si=jzFw0GBHQMJ6wp-A", defaultSettings);
+  let plyr2 = plyrSetup(quickID('SidePlayer1'), "https://www.youtube.com/watch?v=-oyqNzNxLhk", defaultSettings);
+  let plyr3 = plyrSetup(quickID('SidePlayer2'), "https://www.youtube.com/watch?v=K74l26pE4YA", defaultSettings);
+  let plyr4 = plyrSetup(quickID('SidePlayer3'), "https://www.youtube.com/watch?v=Fjp2TdlTTIU", defaultSettings);
+  //divOutliner();
+  let allInputs = document.querySelectorAll(".videoLinkInput");
+  allInputs.forEach((input) => {input.value = "";});
+
+  plyrInputLinker(plyr1, quickID("vidInput1"));
+  plyrInputLinker(plyr2, quickID("vidInput2"));
+  plyrInputLinker(plyr3, quickID("vidInput3"));
+  plyrInputLinker(plyr4, quickID("vidInput4"));
+
+  plyr1.plyr.on("play", () => {hideInput(plyr1)});
+  plyr2.plyr.on("play", () => {hideInput(plyr2)});
+  plyr3.plyr.on("play", () => {hideInput(plyr3)});
+  plyr4.plyr.on("play", () => {hideInput(plyr4)});
+
+  plyr1.plyr.on("pause", () => {showInput(plyr1)});
+  plyr2.plyr.on("pause", () => {showInput(plyr2)});
+  plyr3.plyr.on("pause", () => {showInput(plyr3)});
+  plyr4.plyr.on("pause", () => {showInput(plyr4)});
+
+  quickID('sideOpenBtn').addEventListener('click', showSideBar);
+  quickID('sideCloseBtn').addEventListener('click', hideSideBar);
 });
